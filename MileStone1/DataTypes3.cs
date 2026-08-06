3dfdc0dc260211974b1355dce928a2d84fd405bf
avinashbabuptenali-crypto <avinashbabuptenali@gmail.com>
2026-08-06 09:27:06 +0530

    Add files via upload

---
+++ MileStone1/DataTypes3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string str = Console.ReadLine();
+
+        string result = "";
+
+        foreach (char ch in str)
+        {
+            char newChar = (char)(ch + 1);
+
+            if (char.IsUpper(newChar))
+            {
+                newChar = char.ToLower(newChar);
+            }
+            else if (char.IsLower(newChar))
+            {
+                newChar = char.ToUpper(newChar);
+            }
+
+            result += newChar;
+        }
+
+        Console.WriteLine("New string: " + result);
+    }
+}
