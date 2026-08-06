1fd7a41f2c98f693980ca48a0be955d82252a69f
avinashbabuptenali-crypto <avinashbabuptenali@gmail.com>
2026-08-06 11:52:34 +0530

    Add files via upload

---
+++ MileStone1/FileHandling.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = "Student.txt";
+
+        StreamWriter writer = new StreamWriter(path);
+
+        Console.Write("Enter Student Name: ");
+        string name = Console.ReadLine()!;
+
+        Console.Write("Enter Roll Number: ");
+        string rollNo = Console.ReadLine()!;
+
+        Console.Write("Enter Department: ");
+        string department = Console.ReadLine()!;
+
+        writer.WriteLine("Student Details");
+        writer.WriteLine("----------------------");
+        writer.WriteLine("Name : " + name);
+        writer.WriteLine("Roll Number : " + rollNo);
+        writer.WriteLine("Department : " + department);
+
+        writer.Close();
+
+        Console.WriteLine("\nFile created successfully.");
+    }
+}
