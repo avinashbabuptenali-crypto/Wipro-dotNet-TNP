0b1c5c2570c4ad935101720998d26b2df31f6a16
avinashbabuptenali-crypto <avinashbabuptenali@gmail.com>
2026-08-06 09:33:38 +0530

    Add files via upload

---
+++ MileStone1/ControlStatements2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the first number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the second number (single digit): ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        if (num1 % 10 == num2)
+            Console.WriteLine(num2 + " is in the Unit's place.");
+        else if ((num1 / 10) % 10 == num2)
+            Console.WriteLine(num2 + " is in the Ten's place.");
+        else if ((num1 / 100) % 10 == num2)
+            Console.WriteLine(num2 + " is in the Hundred's place.");
+        else if ((num1 / 1000) % 10 == num2)
+            Console.WriteLine(num2 + " is in the Thousand's place.");
+        else
+            Console.WriteLine(num2 + " is not present in Unit's, Ten's, Hundred's, or Thousand's place.");
+    }
+}
